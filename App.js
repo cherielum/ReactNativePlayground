9698522dd76f91d21c8cc273b26784dd8869772b
Cherie Lum <charismatic.cherie@gmail.com>
2018-03-22 19:17:57 -0400

    delete function

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+
+
 
 export default class App extends Component {
  state = {
-   placeName: ''
- }
+   places: []
+ };
 
- placeNameChangedHandler = (val) => {
-  this.setState({
-    placeName: val
-  })
+ placeAddedHandler = placeName => {
+  this.setState(prevState => {
+    return {
+      places: prevState.places.concat(placeName)
+    };
+  });
+ };
+
+ placeDeletedHandler = index => {
+   this.setState(prevState => {
+     return {
+       places: prevState.places.filter((place, i) => {
+         return i !== index; 
+       })
+     }
+   }); 
  }
 
 
   render() {
     return (
       <View style={styles.container}>
-        <View style ={styles.inputContainer}>
-          <TextInput 
-          style={{width: 300}}
-          placeholder= "An awesome place!"
-          value={this.state.placeName} 
-          onChangeText={this.placeNameChangedHandler}
-          />
-
-          <Button title ="Add" />
-        </View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places} 
+        onItemDeleted= {this.placeDeletedHandler}
+        />
       </View>
     );
   }
@@ -40,10 +51,4 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: "space-between"
-  }
-
 });
