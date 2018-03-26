a6cf0c0d8966b9b401107fb852e3b0e5d0b3a3ca
Cherie Lum <charismatic.cherie@gmail.com>
2018-03-26 18:12:33 -0400

    more redux

--- App.js
+++ App.js
@@ -1,50 +1,26 @@
 import React, {Component} from 'react';
 import { StyleSheet, View } from 'react-native';
+import {connect} from 'react-redux';
 
 import PlaceInput from "./src/components/PlaceInput/PlaceInput";
 import PlaceList from "./src/components/PlaceList/PlaceList";
 //import placeImage from "./src/assets/hawaii.jpg"; //js object
 import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
+import {addPlace, deletePlace, selectPlace, deselectPlace } from './src/store/sctions/index';
 
-
-export default class App extends Component {
- state = {
-   places: [],
-   selectedPlace: null
- };
-
+class App extends Component {
  placeAddedHandler = placeName => {
-  this.setState(prevState => {
-    return {
-      places: prevState.places.concat({
-      key: Math.random(), 
-      name: placeName,
-      image: {
-      //placeImage, 
-        uri: "https://www.getawaytoday.com/image/3759/11683/hawaii-2.jpg"
-      }
-    })
-    };
-  });
+  this.props.onAddPlace(placeName);
  };
 
 
   placeDeletedHandler = () => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.filter(place => {
-          return place.key !== prevState.selectedPlace.key;
-        }),
-        selectedPlace: null
-      };
-    });
+    this.props.onDeletePlace(); 
   };
       
   
   modalClosedHandler = () => {
-    this.setState({
-      selectedPlace: null
-    });
+    this.props.onDeselectPlace(); 
   };
 
   //  this.setState(prevState => {
@@ -57,26 +33,20 @@ export default class App extends Component {
  //}
 
   placeSelectedHandler = key => {
-    this.setState(prevState => {
-      return {
-        selectedPlace: prevState.places.find(place => {
-          return place.key === key;
-        })
-      };
-    });
+    this.props.onSelectPlace(key);
   };
 
   render() {
     return (
       <View style={styles.container}>
         <PlaceDetail
-          selectedPlace={this.state.selectedPlace}
+          selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeletedHandler}
           onModalClosed={this.modalClosedHandler}
         />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
-          places={this.state.places}
+          places={this.props.places}
           onItemSelected={this.placeSelectedHandler}
         />
       </View>
@@ -94,3 +64,22 @@ const styles = StyleSheet.create({
     
   },
 });
+
+//map state to props
+const mapStateToProps = state => {
+  return {
+    places: state.places.places,
+    selectedPlace: state.places.selectedPlace 
+  };
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPlace: (name) => dispatch (addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()), 
+    onSelectPlace: (key) => dispatch(selectPlace(key)),
+    oneDeselectPlace: () => dispatch(deselectPlace())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps) (App); 
--- store/reducers/places.js
+++ store/reducers/places.js
@@ -5,9 +5,47 @@ const initialState = {
 
 const reducer = (state= initialState, action) => {
     switch(action.type) {
-        default: 
+            case ADD_PLACE:
+            return{
+               ...state, 
+               places: state.places.concat({
+                key: Math.random(), 
+                name: action.placeName,
+                image: {
+                //placeImage, 
+                  uri: "https://www.getawaytoday.com/image/3759/11683/hawaii-2.jpg"
+                };
+            })
+        };    
+
+
+        case DELETE_PLACE:
+        return{}
+            ...state. 
+            places: state.places.filter(place => {
+                return place.key !== state.selectedPlace.key;
+              }),
+              selectedPlace: null;
+        };  
+
+        case SELECT_PLACE:
+        return {
+            ...state,
+            selectedPlace: prevState.places.find(place => {
+                return place.key === action.placeKey; //logic to select place 
+              })
+        };
+        case DESELECT_PLACE:
+        return {
+            ...state, 
+            selectedPlace: null 
+        };
+
+     default:   
         return state; 
     }
-}
+};
+
+
 
 export default reducer; 
