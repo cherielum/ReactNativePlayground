8a99235f8bef8f586eee1866f933ee4d434dc7fe
Cherie Lum <charismatic.cherie@gmail.com>
2018-04-03 20:28:57 -0400

    starting to add tabs

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,3 +1,11 @@
 //this will not hold a react component 
 
 import {Navigation } from 'react-native-navigation';
+
+Navigation.startTabBasedApp({
+        tabs: {
+            {
+                screen: 
+            }
+        }
+    }); 
