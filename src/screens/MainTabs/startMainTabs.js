da5862d5e752370790e0f59bee2fe6dbf11a57cd
Cherie Lum <charismatic.cherie@gmail.com>
2018-04-05 13:41:31 -0400

    adding tabs with icons

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,23 +1,33 @@
 //this will not hold a react component 
 
 import {Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 //startTabs is up to you
 const startTabs = () => {
-    Navigation.startTabBasedApp({
-        tabs: [
-            {
-                screen: "awesome-places.FindPlaceScreen",
-                label: "Find Place", 
-                title: "Find Place"
-            },
-            {
-                screen: "awesome-places.SharePlaceScreen",
-                label: "Share Place", 
-                title: "Share Place"
-            }
-        ]
-    }); 
+    Promise.all([
+        Icon.getImageSource("md-map", 30),
+        Icon.getImageSource("ios-share-alt", 30)
+    ]).then(sources => {
+        Navigation.startTabBasedApp({
+            tabs: [
+                {
+                    screen: "awesome-places.FindPlaceScreen",
+                    label: "Find Place", 
+                    title: "Find Place",
+                    icon: sources[0]
+                },
+                {
+                    screen: "awesome-places.SharePlaceScreen",
+                    label: "Share Place", 
+                    title: "Share Place",
+                    icon: sources[1]
+
+                }
+            ]
+        }); 
+    });  
 };
 
 export default startTabs;
