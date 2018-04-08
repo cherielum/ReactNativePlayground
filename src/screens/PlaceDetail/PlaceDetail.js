591c82ebdec1c50c3adf228d07fec5abef623da6
Cherie Lum <charismatic.cherie@gmail.com>
2018-04-08 13:22:51 -0400

    fixing navigating backwards code

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,12 +1,13 @@
 import React, {Component} from "react";
-import { View, Image, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
-import {connect} from 'react-redux'; 
+import { View, Image, Text, Button, StyleSheet, TouchableOpacity } 
+  from "react-native";
+import { connect } from 'react-redux'; 
 import Icon from 'react-native-vector-icons/Ionicons';
-import {onDeletePlace} from '../../store/actions/index';
+import { deletePlace } from '../../store/actions/index';
 
 class PlaceDetail extends Component {
   placeDeletedHandler = () => {
-    this.onDeletePlace(this.props.selectedPlace.key);
+    this.props.onDeletePlace(this.props.selectedPlace.key);
     this.props.navigator.pop();
   }
 
@@ -18,7 +19,7 @@ class PlaceDetail extends Component {
         <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
       </View>
       <View>
-        <TouchableOpacity onPress ={props.onItemDeleted} > 
+        <TouchableOpacity onPress ={this.placeDeletedHandler} > 
         <View style = {styles.deleteButton}>
           <Icon size={30} name="ios-trash" color="red"/>
         </View>
