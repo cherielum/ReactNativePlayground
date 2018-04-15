007d4db0ca5070af448608f2615f6162c8163d4f
Cherie Lum <charismatic.cherie@gmail.com>
2018-04-15 12:06:13 -0400

    styling Share Place screen

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,38 +1,44 @@
 import React, {Component } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView, Image } from 'react-native';
 
 //middleware
 import { connect } from 'react-redux';
 import { addPlace } from '../../store/actions/index';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import MainText from "../../components/UI/MainText/MainText";
+import HeadingText from "../../components/UI/HeadingText/HeadingText";
+
+import imagePlaceholder from "../../assets/hawaii.jpg";
 
 class SharePlaceScreen extends Component {
     constructor(props) {
-        super(props); 
+        super(props);
         this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
     }
- 
+
     onNavigatorEvent = event => {
         if (event.type === "NavBarButtonPress") {
             if (event.id === "sideDrawerToggle") {
                 this.props.navigator.toggleDrawer({
                     side: "left"
                 })
-            } 
-        }  
+            }
+        }
     }
 
     placeAddedHandler = placeName => {
-        this.props.onAddPlace(placeName); 
+        this.props.onAddPlace(placeName);
     }
 
     render() {
         return (
             <ScrollView>
             <View style={styles.container}>
-                <Text>Share a Place with Us! </Text>
+                <MainText>
+                    <HeadingText> Share a Place with Us! </HeadingText>
+                </MainText>
                 <View style={styles.placeholder}>
-                    <Text>Image Preview </Text>
+                   <Image source={imagePlaceholder} style={styles.previewImage}/>
                 </View>
                 <View style={styles.button}>
                     <Button title="Pick Image" />
@@ -67,7 +73,11 @@ const styles = StyleSheet.create({
         height: 150
     },
     button: {
-       margin: 8 
+       margin: 8
+    },
+    previewImage:{
+        width: "100%",
+        height: "100%"
     }
 })
 
@@ -77,4 +87,4 @@ const mapDispatchToProps = dispatch =>{
 
     };
 };
-export default connect(null, mapDispatchToProps) (SharePlaceScreen); 
+export default connect(null, mapDispatchToProps) (SharePlaceScreen);
