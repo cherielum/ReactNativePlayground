1e91c5a9fdf0e55bc1badcc6faca109d162ef9b5
Cherie Lum <charismatic.cherie@gmail.com>
2018-04-17 09:35:52 -0400

    style to slides

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { View, TextInput, Button, StyleSheet } from "react-native";
 import DefaultInput from "../UI/DefaultInput/DefaultInput";
 
-placeInput = props => (
+const placeInput = props => (
   <DefaultInput
     placeholder="Place Name"
     value={props.placeName}
